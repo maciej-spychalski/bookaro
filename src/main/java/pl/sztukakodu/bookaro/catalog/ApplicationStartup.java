d2746b55c08e2ffe2c7b3d04cb7cec6fc517acdc
maciej.spychalski <maciej.spychalski@gmail.com>
2021-11-01 15:51:24 +0100

    M2L10 - Lombok

--- src/main/java/pl/sztukakodu/bookaro/catalog/ApplicationStartup.java
+++ src/main/java/pl/sztukakodu/bookaro/catalog/ApplicationStartup.java
@@ -1,5 +1,6 @@
 package pl.sztukakodu.bookaro.catalog;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.sztukakodu.bookaro.catalog.application.CatalogController;
@@ -8,13 +9,9 @@ import pl.sztukakodu.bookaro.catalog.domain.Book;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ApplicationStartup implements CommandLineRunner {
-
     private final CatalogController catalogController;
-
-    public ApplicationStartup(CatalogController catalogController) {
-        this.catalogController = catalogController;
-    }
 
     @Override
     public void run(String... args) throws Exception {
