73f03db1878aea99867c31e8cc524228a032def2
maciej.spychalski <maciej.spychalski@gmail.com>
2021-11-02 12:42:49 +0100

    M2L12 - Wstrzykiwanie wartosci prymitywnych

--- src/main/java/pl/sztukakodu/bookaro/catalog/ApplicationStartup.java
+++ src/main/java/pl/sztukakodu/bookaro/catalog/ApplicationStartup.java
@@ -1,6 +1,6 @@
 package pl.sztukakodu.bookaro.catalog;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.sztukakodu.bookaro.catalog.application.CatalogController;
@@ -9,13 +9,23 @@ import pl.sztukakodu.bookaro.catalog.domain.Book;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class ApplicationStartup implements CommandLineRunner {
     private final CatalogController catalogController;
+    private final String title;
+    private final Long limit;
+
+    public ApplicationStartup(
+            CatalogController catalogController,
+            @Value("${bookaro.catalog.query:Pan}") String title,
+            @Value("${bookaro.catalog.limit:3}") Long limit) {
+        this.catalogController = catalogController;
+        this.title = title;
+        this.limit = limit;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        List<Book> books = catalogController.findByTitle("Pan");
-        books.forEach(System.out::println);
+        List<Book> books = catalogController.findByTitle(title);
+        books.stream().limit(limit).forEach(System.out::println);
     }
 }
