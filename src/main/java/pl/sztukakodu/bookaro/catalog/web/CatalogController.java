b469bf8c483e3430a45352aa0f0b89df0285b64f
maciej.spychalski <maciej.spychalski@gmail.com>
2021-11-15 17:32:16 +0100

    M4L7 - Walidacja

--- src/main/java/pl/sztukakodu/bookaro/catalog/web/CatalogController.java
+++ src/main/java/pl/sztukakodu/bookaro/catalog/web/CatalogController.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase;
 import pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase.CreateBookCommand;
 import pl.sztukakodu.bookaro.catalog.domain.Book;
 
+import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
@@ -28,29 +33,25 @@ public class CatalogController {
             @RequestParam Optional<String> author) {
         if (title.isPresent() && author.isPresent()) {
             return catolog.findByTitleAndAuthor(title.get(), author.get());
-        }
-        else if (title.isPresent()) {
+        } else if (title.isPresent()) {
             return catolog.findByTitle(title.get());
-        }
-        else if (author.isPresent()) {
+        } else if (author.isPresent()) {
             return catolog.findByAuthor(author.get());
         }
         return catolog.findAll();
     }
 
-        @GetMapping("/{id}")
-//    public ResponseEntity<?> getById(@PathVariable Long id) {
+    @GetMapping("/{id}")
     public ResponseEntity<Book> getById(@PathVariable Long id) {
         return catolog
                 .findById(id)
-//                .map(book -> ResponseEntity.ok(book))
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Void> addBook(@RequestBody RestCreateBookCommand command) {
+    public ResponseEntity<Void> addBook(@Valid @RequestBody RestCreateBookCommand command) {
         Book book = catolog.addBook(command.toCommand());
         return ResponseEntity.created(createdBookUri(book)).build();
     }
@@ -68,9 +69,14 @@ public class CatalogController {
 
     @Data
     private static class RestCreateBookCommand {
+        @NotBlank
         private String title;
+        @NotBlank
         private String author;
+        @NotNull
         private Integer year;
+        @NotNull
+        @DecimalMin("0.00")
         private BigDecimal price;
 
         CreateBookCommand toCommand() {
