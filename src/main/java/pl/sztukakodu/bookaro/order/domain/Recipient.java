dd3fdedfa609727a4399417eb9e3db0abdadccd7
maciej.spychalski <maciej.spychalski@gmail.com>
2021-11-26 13:29:41 +0100

    M5L9 - Many To One

--- src/main/java/pl/sztukakodu/bookaro/order/domain/Recipient.java
+++ src/main/java/pl/sztukakodu/bookaro/order/domain/Recipient.java
@@ -4,19 +4,44 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Embeddable;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Embeddable
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Recipient {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private String phone;
     private String street;
     private String city;
     private String zipCode;
     private String email;
+
+
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+    public Recipient(String name, String phone, String street, String city, String zipCode, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.email = email;
+    }
 }
