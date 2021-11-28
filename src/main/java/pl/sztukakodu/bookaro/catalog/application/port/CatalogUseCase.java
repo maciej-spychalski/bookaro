b3e7a695d5785e28377d8f5ef027a88d3f6a3ad1
maciej.spychalski <maciej.spychalski@gmail.com>
2021-11-28 21:01:21 +0100

    M5L14 - Zaawansowane zapytania JPA

--- src/main/java/pl/sztukakodu/bookaro/catalog/application/port/CatalogUseCase.java
+++ src/main/java/pl/sztukakodu/bookaro/catalog/application/port/CatalogUseCase.java
@@ -25,8 +25,6 @@ public interface CatalogUseCase {
 
     List<Book> findByTitleAndAuthor(String title, String author);
 
-    Optional<Book> findOneByTitleAndAuthor(String title, String author);
-
     Book addBook(CreateBookCommand command);
 
     void removeById(long id);
