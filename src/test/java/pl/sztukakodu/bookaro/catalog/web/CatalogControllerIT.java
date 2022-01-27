dfbc4ebb3af7ab1ba3819c927d91bc743dfb93fc
maciej.spychalski <maciej.spychalski@gmail.com>
2022-01-27 16:13:59 +0100

    M8L03 - Testowanie w kontekcie Springa – @SpringBootTest

---
+++ src/test/java/pl/sztukakodu/bookaro/catalog/web/CatalogControllerIT.java
@@ -0,0 +1,60 @@
+package pl.sztukakodu.bookaro.catalog.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase;
+import pl.sztukakodu.bookaro.catalog.db.AuthorJpaRepository;
+import pl.sztukakodu.bookaro.catalog.domain.Author;
+import pl.sztukakodu.bookaro.catalog.domain.Book;
+
+import java.math.BigDecimal;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase.*;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+class CatalogControllerIT {
+
+    @Autowired
+    AuthorJpaRepository authorJpaRepository;
+
+    @Autowired
+    CatalogUseCase catalogUseCase;
+
+    @Autowired
+    CatalogController controller;
+
+
+    @Test
+    public void getAllBooks() {
+        // Given
+        Author goetz = authorJpaRepository.save(new Author("Brian Goetz"));
+        Author bloch = authorJpaRepository.save(new Author("Joshua Bloch"));
+        catalogUseCase.addBook(new CreateBookCommand(
+                "Effective Java",
+                Set.of(bloch.getId()),
+                2007,
+                new BigDecimal("119.00"),
+                50L
+        ));
+        catalogUseCase.addBook(new CreateBookCommand(
+                "Java Concurrency in Practice",
+                Set.of(goetz.getId()),
+                2004,
+                new BigDecimal("66.49"),
+                50L
+        ));
+        // When
+        List<Book> all = controller.getAll(Optional.empty(), Optional.empty());
+        // Then
+        assertEquals(2, all.size());
+    }
+
+}
