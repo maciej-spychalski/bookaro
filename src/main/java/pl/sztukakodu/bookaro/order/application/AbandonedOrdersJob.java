e2b5293f4d12a1e55ab92e316bf8767d29923731
maciej.spychalski <maciej.spychalski@gmail.com>
2022-02-03 20:07:22 +0100

    M8L17 - Test @Scheduled, nadpisywanie propertiesów i beanów

--- src/main/java/pl/sztukakodu/bookaro/order/application/AbandonedOrdersJob.java
+++ src/main/java/pl/sztukakodu/bookaro/order/application/AbandonedOrdersJob.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import pl.sztukakodu.bookaro.clock.Clock;
 import pl.sztukakodu.bookaro.order.application.port.ManipulateOrderUseCase;
 import pl.sztukakodu.bookaro.order.db.OrderJpaRepository;
 import pl.sztukakodu.bookaro.order.domain.Order;
@@ -23,17 +24,18 @@ public class AbandonedOrdersJob {
     private final OrderJpaRepository repository;
     private final ManipulateOrderUseCase orderUseCase;
     private final OrdersProperties properties;
+    private final Clock clock;
 
     @Scheduled(cron = "${app.orders.abandon-cron}")
     @Transactional
     public void run() {
         Duration paymentPeriod = properties.getPaymentPeriod();
-        LocalDateTime olderThan = LocalDateTime.now().minus(paymentPeriod);
+        LocalDateTime olderThan = clock.now().minus(paymentPeriod);
         List<Order> orders = repository.findByStatusAndCreatedAtLessThanEqual(OrderStatus.NEW, olderThan);
         log.info("Found orders to be abandoned: " + orders.size());
         orders.forEach(order -> {
             // TODO-Maciek: naprawić w module security
-            String adminEmail = "adming@example.org";
+            String adminEmail = "admin@example.org";
             UpdateStatusCommand command = new UpdateStatusCommand(order.getId(), OrderStatus.ABANDONED, adminEmail);
             orderUseCase.updateOrderStatus(command);
         });
