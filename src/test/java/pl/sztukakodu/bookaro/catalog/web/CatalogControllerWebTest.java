514ead7b22ab5bdcb52f7e3b237ba7f005f795b2
maciej.spychalski <maciej.spychalski@gmail.com>
2022-02-09 12:25:51 +0100

    M9L11 - Polaczenie z baza danych

--- src/test/java/pl/sztukakodu/bookaro/catalog/web/CatalogControllerWebTest.java
+++ src/test/java/pl/sztukakodu/bookaro/catalog/web/CatalogControllerWebTest.java
@@ -5,6 +5,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase;
 import pl.sztukakodu.bookaro.catalog.domain.Book;
